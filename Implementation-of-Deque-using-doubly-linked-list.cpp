f336bd1947420e9e51c8e2959b6fe281b95d87b5
Manan <kinger2225@gmail.com>
2020-08-03 00:36:00 +0530

    Deque using DLL

---
+++ Implementation-of-Deque-using-doubly-linked-list.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+using namespace std;
+class node{
+	public:
+		int data;
+		node *next;
+		node *prev;
+		node(int value){
+			data=value;
+			next=prev=NULL;
+		}
+};
+class deque{
+	node *front;
+	node *rear;
+	int deqsize;
+	public:
+		deque(){
+			front=rear=NULL;
+			deqsize=0;
+		}
+		void insertFront(int v);
+		void insertRear(int v);
+		void deleteFront();
+		void deleteRear();
+		int getFront();
+		int getRear();
+		bool isempty();
+		int size();
+		void erase();
+};
+main(){
+	deque dq;
+	dq.insertFront(2);
+	dq.insertRear(34);
+	dq.deleteFront();
+	dq.deleteFront();
+	dq.getRear();
+}
+void deque::insertFront(int v){
+	if(front==NULL){
+		front=new node(v);
+		rear=front;
+	}
+	else{
+		node *p = new node(v);
+		p->next=front;
+		front->prev=p;
+		front = p;
+	}
+		++deqsize;
+}
+void deque::insertRear(int v){
+	if(rear==NULL){
+		rear = new node(v);
+		front  = rear;
+	}
+	else{
+		node *p = new node(v);
+		rear->next=p;
+		p->prev=rear;
+		rear=p;
+	}
+	++deqsize;
+}
+void deque::deleteFront(){
+	if(front!=NULL){
+		front  = front->next;
+		if(front==NULL){
+			rear=NULL;
+		}
+		--deqsize;
+	}
+}
+void deque::deleteRear(){
+	if(rear!=NULL){
+		rear=rear->prev;
+		if(rear==NULL){
+			front=NULL;
+		}
+		--deqsize;
+	}
+}
+void deque::erase(){
+	front = rear = NULL;
+	deqsize=0;
+}
+bool deque::isempty(){
+	if(front==NULL && rear==NULL){
+		return true;
+	}
+	return false;
+}
+int deque::getFront(){
+	if(front!=NULL){
+		return front->data;
+	}
+}
+int deque::getRear(){
+	if(rear!=NULL){
+		return rear->data;
+	}
+}
+int deque::size(){
+	return deqsize;
+}
